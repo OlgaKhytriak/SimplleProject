4600fb98125c71b5123d160acb22a2164a5c9adb
OlgaKhytriak <KhytriakOlga@gmail.com>
2017-10-11 23:21:26 +0300

    Changed modulus operators

--- TasksFromYurijL/src/SimpleTasks/NumberCheck.java
+++ TasksFromYurijL/src/SimpleTasks/NumberCheck.java
@@ -5,14 +5,12 @@ public class NumberCheck {
 	 *  @return Check if the number is odd
 	*/
 	public boolean isOddNumber(int number) {
-		if(0!=number%2) return true;
-		else return false;
+		return (number&1)==1;
 	}
 	/**
 	 *  @return Check if the number is even
 	*/
 	public boolean isEvenNumber(int number) {
-		if(0==number%2) return true;
-		else return false;
+		return (number&1)==0;
 	}
 }
